6a446b0591c8f367701add8189bc0d280a5a68d1
Melkeydev <amokstakov@gmail.com>
2025-12-26 00:21:57 -0800

    day 1 part 2 done

--- day_two/src/main.zig
+++ day_two/src/main.zig
@@ -1,27 +1,63 @@
 const std = @import("std");
-const day_two = @import("day_two");
+var stdout_buffer: [1024]u8 = undefined;
+var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+const stdout = &stdout_writer.interface;
+const fs = std.fs;
 
+// Day 1
+//
+// first step read an input file
+// if/else statement keeping track of first character for either L or R
+// edge cases
+// starts at 50
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try day_two.bufferedPrint();
+    var dial_position: i32 = 50;
+    var counter: i32 = 0;
+
+    var file = try open_file();
+    defer file.close();
+
+    try read_file(file, &dial_position, &counter);
+
+    try stdout.print("final position value - {d}\n", .{dial_position});
+    try stdout.print("the counter is - {d}\n", .{counter});
+    try stdout.flush();
 }
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+fn open_file() !fs.File {
+    const file = try std.fs.cwd().openFile("inputs.txt", .{});
+    return file;
 }
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+fn read_file(file: fs.File, dial_position: *i32, counter: *i32) !void {
+    var buffer: [1024]u8 = undefined;
+    var file_reader = file.reader(&buffer);
+    const reader = &file_reader.interface;
+
+    while (reader.takeDelimiterExclusive('\n')) |line| {
+        const trimmed_line = std.mem.trimRight(u8, line, "\r");
+        try process_code(trimmed_line, dial_position, counter);
+    } else |err| {
+        if (err != error.EndOfStream) return err;
+    }
+}
+
+fn process_code(code: []const u8, dial_position: *i32, counter: *i32) !void {
+    const direction = code[0];
+    const value = try std.fmt.parseInt(i32, code[1..], 10);
+    const start = dial_position.*;
+
+    if (direction == 'R') {
+        // Count how many times we pass through 0
+        counter.* += @divTrunc(start + value, 100);
+        dial_position.* = @mod(start + value, 100);
+    } else {
+        // For L direction
+        if (start == 0) {
+            counter.* += @divTrunc(value, 100);
+        } else if (value >= start) {
+            counter.* += 1 + @divTrunc(value - start, 100);
         }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+        dial_position.* = @mod(start - value, 100);
+    }
 }
